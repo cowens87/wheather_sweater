9bcf209bd67e2dd80714259ade06a328a0fff304
Cydnee Owens <cks410@yahoo.com>
2021-03-08 09:11:05 -0800

    update munchies structure test

--- spec/requests/api/v1/munchies_request_spec.rb
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -23,14 +23,13 @@ RSpec.describe 'Yelp API Endpoint', :vcr do
   
       expect(munchie[:id]).to be_nil
       expect(munchie[:type]).to eq('munchie')
-      check_hash_structure(munchie, :id, String)
       check_hash_structure(munchie, :type, String)
       check_hash_structure(munchie_attr, :destination_city, String)
       check_hash_structure(munchie_attr, :travel_time, String)
       check_hash_structure(munchie_attr, :forecast, Hash)
       check_hash_structure(munchie_attr, :restaurant, Hash)
       check_hash_structure(munchie_frcst, :summary, String)
-      check_hash_structure(munchie_frcst, :temperature, Integer)
+      check_hash_structure(munchie_frcst, :temperature, Float)
       check_hash_structure(munchie_rest, :name, String)
       check_hash_structure(munchie_rest, :address, String)
     end
