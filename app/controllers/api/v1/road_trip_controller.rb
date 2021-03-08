c72f642e83475d07ff2d56812fd61b6bd0a142b2
Cydnee Owens <cks410@yahoo.com>
2021-03-08 06:59:30 -0800

    update method naming conventions

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,36 +1,28 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
     user = User.find_by(api_key: road_trip_params[:api_key])
-    if user && (find_empty_values(road_trip_params) == false)
+    if user && (has_req_values?(road_trip_params) == false)
       roadtrip = RoadTripFacade.get_trip(road_trip_params)
       render json: RoadtripSerializer.new(roadtrip)
-    elsif find_empty_values(road_trip_params)
-      render json: { body: generate_error }, status: 404
-    elsif bad_api_key(road_trip_params, user)
-      render json: { body: api_error }, status: 401
+    elsif has_req_values?(road_trip_params)
+      render json: { body: 'Missing/empty fields. Please try again' }, status: 404
+    elsif invalid_api_key?(road_trip_params, user)
+      render json: { body: 'Missing/incorrect API key. Please try again' }, status: 401
     end
   end
 
   private
 
-  def find_empty_values(road_trip_params)
+  def road_trip_params
+    params.permit(:origin, :destination, :api_key)
+  end
+
+  def has_req_values?(road_trip_params)
     road_trip_params[:origin].empty? || road_trip_params[:destination].empty?
   end
 
-  def bad_api_key(road_trip_params, user)
+  def invalid_api_key?(road_trip_params, user)
     user.nil? || road_trip_params[:api_key].empty?
     return true
-  end
-
-  def api_error
-    'Missing/incorrect API key. Please try again'
-  end
-
-  def generate_error
-    'Missing/empty fields. Please try again'
-  end
-
-  def road_trip_params
-    params.permit(:origin, :destination, :api_key)
   end
 end
