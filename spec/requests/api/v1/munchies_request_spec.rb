7117515d91e70f5e8ad6f6dad38fb4eef5d32bc0
Cydnee Owens <cks410@yahoo.com>
2021-03-08 09:09:40 -0800

    add munchies structure test

--- spec/requests/api/v1/munchies_request_spec.rb
+++ spec/requests/api/v1/munchies_request_spec.rb
@@ -2,20 +2,37 @@
 require 'rails_helper'
 
 RSpec.describe 'Yelp API Endpoint', :vcr do
-  it 'can find food recommendation and weather for destination' do
-    munchies_params = {
-                        origin: 'Denver, CO',
-                        destination: 'Pueblo, CO',
-                        food: 'hamburger'
-                      }
-    headers         = {
-                        'CONTENT_TYPE': 'application/json',
-                        'Authorization': "Bearer #{ENV['YELP_API_KEY']}"
-                      }
-
-    get '/api/v1/munchies', headers: headers, params: munchies_params
-
-    munchie = JSON.parse(response.body, symbolize_names: true)[:data]
-require 'pry'; binding.pry
-
+  describe 'happy paths' do
+    it 'can find food recommendation and weather for destination' do
+      munchies_params = {
+                          origin: 'Denver, CO',
+                          destination: 'Pueblo, CO',
+                          food: 'hamburger'
+                        }
+      headers         = {
+                          'CONTENT_TYPE': 'application/json',
+                          'Authorization': "Bearer #{ENV['YELP_API_KEY']}"
+                        }
+  
+      get '/api/v1/munchies', headers: headers, params: munchies_params
+  
+      munchie       = JSON.parse(response.body, symbolize_names: true)[:data]
+      munchie_attr  = munchie[:attributes]
+      munchie_frcst = munchie[:attributes][:forecast]
+      munchie_rest  = munchie[:attributes][:restaurant]
+  
+      expect(munchie[:id]).to be_nil
+      expect(munchie[:type]).to eq('munchie')
+      check_hash_structure(munchie, :id, String)
+      check_hash_structure(munchie, :type, String)
+      check_hash_structure(munchie_attr, :destination_city, String)
+      check_hash_structure(munchie_attr, :travel_time, String)
+      check_hash_structure(munchie_attr, :forecast, Hash)
+      check_hash_structure(munchie_attr, :restaurant, Hash)
+      check_hash_structure(munchie_frcst, :summary, String)
+      check_hash_structure(munchie_frcst, :temperature, Integer)
+      check_hash_structure(munchie_rest, :name, String)
+      check_hash_structure(munchie_rest, :address, String)
+    end
+  end
 end
